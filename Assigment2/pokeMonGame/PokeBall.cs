1e9defc47f18d0d0925e7bb3ee25745d3beddfc8
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2024-03-05 16:04:19 +0100

    Added assigment 2

---
+++ Assigment2/pokeMonGame/PokeBall.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokeMonGame
+{
+    public class PokeBall
+    {
+
+        public Charmander charmander;
+
+        public PokeBall(Charmander charmander)
+        {
+
+            this.charmander = charmander;
+
+        }
+
+
+        public bool isFull()
+        {
+            if (charmander == null) 
+                return false;
+
+            return true;
+        }
+
+        public string openPokeball()
+        {
+            if (!isFull())
+                return "Pokeball is empty";
+
+            return $"{charmander.name} is released : {charmander.battleCry()}";
+        }
+
+        public string closePokeball()
+        {
+            if (!isFull())
+                return "Pokeball is empty";
+
+            return $"{charmander.name} is back to the pokeball";
+        }
+
+    }
+}
---
+++ Assigment2/pokeMonGame/Trainer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokeMonGame
+{
+    public class Trainer
+    {
+        public string name;
+
+        public List<PokeBall> belt;
+        public Trainer(string name)
+        {
+
+            this.name = name;
+            belt = new List<PokeBall>();
+
+        }
+
+        public string addPokeballsTotheBelt(string name)
+        {
+            if (belt.Count <= 6)
+            {
+                Charmander charmander = new Charmander(name);
+                PokeBall pokeBall = new PokeBall(charmander);
+                belt.Add(pokeBall);
+                return "Pokeball added successfully";
+            }
+
+            return "Trainer belt is full";
+        }
+
+        public string throwPokeball(int index)
+        {
+            return $"Trainer {name} throw his pokeball {belt[index].openPokeball()}";
+        }
+
+        public string returnPokeball(int index)
+        {
+            return $"Trainer {name} return his pokeball  back : {belt[index].closePokeball()}";
+        }
+    }
+}
