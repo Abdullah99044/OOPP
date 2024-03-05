7095613fcb7b7b94a4301921a72ca72de9bb02f4
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2024-03-05 14:13:40 +0100

    Assigment 1

---
+++ pokeMonGame/pokeMonGame/Charmander.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokeMonGame
+{
+    public class Charmander
+    {
+        public string name;
+        public string strength;
+        public string weakness;
+
+        public Charmander(string name)
+        {
+            this.name = name;
+            strength = "Fire";
+            weakness = "Water";
+        }
+
+        public string battleCry()
+        {
+            return $"Go {name}!";
+        }
+    }
+}
---
+++ pokeMonGame/pokeMonGame/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+
+
+
+
+using pokeMonGame;
+
+Console.WriteLine("Do you want to play ? : (yes/no) ");
+string answer = Console.ReadLine();
+
+Console.WriteLine("");
+Console.WriteLine("************************************************");
+Console.WriteLine("");
+
+if (answer == "Yes" || answer == "yes")
+{
+    while (true)
+    {
+
+        Console.WriteLine("Give your a charmander a name : ");
+        string name = Console.ReadLine();
+
+        Charmander pokemone = new Charmander(name);
+        Console.WriteLine("");
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(pokemone.battleCry());
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("************************************************");
+        
+        Console.WriteLine("Do you want to quit ? : (yes/no) ");
+        answer = Console.ReadLine();
+        Console.WriteLine("");
+
+        if (answer == "yes" || answer == "Yes")
+            break;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("************************************************");
+Console.WriteLine("");
+Console.WriteLine("Good bye");
+
+
