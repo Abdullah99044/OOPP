3c8aea128bccfce5c2e3e4ebda57a8ee2d45a847
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2024-03-06 11:46:06 +0100

    Assigment 3

---
+++ Assigment 3/pokeMonGame/pokeMonGame/Charmander.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokeMonGame
+{
+    public class Charmander : Pokemon
+    {
+       
+        public Charmander(string name) : base(name)
+        {
+            this.name = name;
+            strength = "Fire";
+            weakness = "Water";
+ 
+        }
+
+
+
+        public override string battleCry()
+        {
+            return $"Go {name}!";
+        }
+    }
+}
---
+++ Assigment 3/pokeMonGame/pokeMonGame/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+
+
+
+
+using pokeMonGame;
+
+Console.WriteLine("Do you want to play ? : (yes/no) ");
+string answer = Console.ReadLine();
+
+Console.WriteLine("");
+Console.WriteLine("************************************************");
+Console.WriteLine("");
+
+if (answer == "Yes" || answer == "yes")
+{
+    while (true)
+    {
+
+        Console.WriteLine("Give Trainer 1 a name : ");
+        string trainer1Name = Console.ReadLine();
+
+        Console.WriteLine("Give Trainer 2 a name : ");
+        string trainer2Name = Console.ReadLine();
+
+        Trainer trainer1 = new Trainer(trainer1Name);
+        Trainer trainer2 = new Trainer(trainer2Name);
+
+        Console.WriteLine("");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("");
+
+        if (trainer1.belt.Count <= 6)
+        {
+            for (int i = 0; i != 6; i++)
+            {
+                Console.WriteLine($"Give Pokemone {i+1} of Trainer {trainer1.name} a name : ");
+                string charmanderName = Console.ReadLine();
+                Console.WriteLine(trainer1.addPokeballsTotheBelt(charmanderName , i));
+               ;
+            }
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("");
+
+        if (trainer2.belt.Count <= 6)
+        {
+            for (int i = 0; i != 6; i++)
+            {
+                Console.WriteLine($"Give Pokemone {i+1} of Trainer {trainer2.name} a name : ");
+                string charmanderName = Console.ReadLine();
+                Console.WriteLine(trainer2.addPokeballsTotheBelt(charmanderName, i));
+            }
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("");
+
+        int index = 0;
+        while(index != 6)
+        {
+           
+            Console.WriteLine(trainer1.throwPokeball(index));
+            Console.WriteLine(trainer2.throwPokeball(index));
+
+            Console.WriteLine("");
+            Console.WriteLine(trainer1.returnPokeball(index));
+            Console.WriteLine(trainer2.returnPokeball(index));
+            Console.WriteLine("");
+
+            index++;
+        }
+        
+
+        Console.WriteLine("");
+        Console.WriteLine("************************************************");
+        Console.WriteLine("");
+
+        Console.WriteLine("Do you want to quit ? : (yes/no) ");
+        answer = Console.ReadLine();
+        Console.WriteLine("");
+
+        if (answer == "yes" || answer == "Yes")
+            break;
+    }
+}
+
+Console.WriteLine("");
+Console.WriteLine("************************************************");
+Console.WriteLine("");
+Console.WriteLine("Good bye");
+
+
